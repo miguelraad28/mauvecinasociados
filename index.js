ef0d4f204b4e7b084cc88f518d621c48ee6ca8bd
miguelraad28 <miguelraad2020@gmail.com>
2022-11-21 15:17:44 -0300

    falta contacto

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const navbar = document.getElementById("navbar")
 window.addEventListener("scroll", function(){
-    navbar.classList.toggle("scrollDown", window.scrollY > 100);
+    navbar.classList.toggle("scrollDown", window.scrollY > 0);
 })
