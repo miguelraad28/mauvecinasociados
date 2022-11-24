916321cad73e1fbb0009c6f3bf4affd6cb66c379
miguelraad28 <miguelraad2020@gmail.com>
2022-11-24 17:10:23 -0300

    scroll margin not working

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const navbarContainer = document.getElementById("navbarContainer")
+window.addEventListener("scroll", function () {
+    navbarContainer.classList.toggle("scrollDown", window.scrollY > 0);
+})
 const navbar = document.getElementById("navbar")
 window.addEventListener("scroll", function () {
-    navbar.classList.toggle("scrollDown", window.scrollY > 0);
+    navbar.classList.toggle("scrollDownShadow", window.scrollY > 0);
 })
