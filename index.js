fc03e5de8621c24ea6da7d1da82645246e3f06ea
miguelraad28 <miguelraad2020@gmail.com>
2022-11-26 14:44:09 -0300

    Mobile Navbar closes OK

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const navbarContainer = document.getElementById("navbarContainer")
 window.addEventListener("scroll", function () {
-    navbarContainer.classList.toggle("scrollDown", window.scrollY > 0);
+    navbarContainer.classList.toggle("scrollDownPadding", window.scrollY > 0);
 })
 const navbar = document.getElementById("navbar")
 window.addEventListener("scroll", function () {
-    navbar.classList.toggle("scrollDownShadow", window.scrollY > 0);
+    navbar.classList.toggle("scrollDown", window.scrollY > 0);
+})
+// Cierre propio para el mobileNavbar (offCanvas) debido a que el data-bs-dismiss de bootstrap no me permite realizar la navegación a través de la página, cierra el offCanvas y la navegación queda a medias.
+const offCanvas = document.getElementById("offCanvas")
+document.querySelectorAll('.mobileNavbarButton').forEach(item => {
+    item.addEventListener('click', () => {
+        document.body.removeAttribute("style")
+        const offCanvasBackdrop = document.querySelector(".offcanvas-backdrop")
+        offCanvas.classList.toggle("hiding")
+        offCanvasBackdrop.classList.toggle("show")
+        offCanvasBackdrop.remove()
+        setTimeout(() => {
+            offCanvas.removeAttribute("aria-modal")
+            offCanvas.removeAttribute("role")
+            offCanvas.classList.toggle("hiding")
+            offCanvas.classList.toggle("show")
+        }, 1000);
+
+    })
 })
